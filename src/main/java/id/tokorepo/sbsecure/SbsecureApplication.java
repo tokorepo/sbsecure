017309d5e3df4e124cbb8c4aa192a707db6783d3
BOEING <dimsoldev@gmail.com>
2025-07-03 00:19:25 +0700

    init commit

---
+++ src/main/java/id/tokorepo/sbsecure/SbsecureApplication.java
@@ -0,0 +1,12 @@
+package id.tokorepo.sbsecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsecureApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbsecureApplication.class, args);
+  }
+}
---
+++ src/test/java/id/tokorepo/sbsecure/SbsecureApplicationTests.java
@@ -0,0 +1,11 @@
+package id.tokorepo.sbsecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbsecureApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
